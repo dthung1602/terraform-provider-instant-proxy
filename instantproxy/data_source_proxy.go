9963bf4a2f95ceee6099b7e831419e95baeea34b
dthung1602 <dthung1602@gmail.com>
2022-06-05 18:49:21 +0700

    Proxy data source

---
+++ instantproxy/data_source_proxy.go
@@ -0,0 +1,66 @@
+package instantproxy
+
+import (
+	"context"
+	"strconv"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	ipac "github.com/dthung1602/instant-proxy-api-client"
+)
+
+func dataSourceProxies() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: datasourceProxiesRead,
+		Schema: map[string]*schema.Schema{
+			"proxies": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"value": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"ip": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"port": &schema.Schema{
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func datasourceProxiesRead(ctx context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*ipac.Client)
+	proxies, err := client.GetProxies()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	result := make([]*map[string]interface{}, len(proxies), len(proxies))
+	for i, proxy := range proxies {
+		result[i] = &map[string]interface{}{
+			"value": proxy.String(),
+			"ip":    proxy.IP.String(),
+			"port":  proxy.Port,
+		}
+	}
+
+	if err := data.Set("proxies", result); err != nil {
+		return diag.FromErr(err)
+	}
+
+	// always run
+	data.SetId(strconv.FormatInt(time.Now().Unix(), 10))
+
+	return diag.Diagnostics{}
+}
--- instantproxy/provider.go
+++ instantproxy/provider.go
@@ -1,12 +1,49 @@
 package instantproxy
 
 import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	ipac "github.com/dthung1602/instant-proxy-api-client"
 )
 
 func Provider() *schema.Provider {
 	return &schema.Provider{
-		ResourcesMap:   map[string]*schema.Resource{},
-		DataSourcesMap: map[string]*schema.Resource{},
+		Schema:               providerSchema(),
+		ConfigureContextFunc: configureContext,
+		ResourcesMap:         map[string]*schema.Resource{},
+		DataSourcesMap: map[string]*schema.Resource{
+			"instantproxy_proxies": dataSourceProxies(),
+		},
 	}
+}
+
+func providerSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"username": &schema.Schema{
+			Type:        schema.TypeString,
+			Optional:    true,
+			DefaultFunc: schema.EnvDefaultFunc("INSTANT_PROXY_USERNAME", nil),
+		},
+		"password": &schema.Schema{
+			Type:        schema.TypeString,
+			Sensitive:   true,
+			Optional:    true,
+			DefaultFunc: schema.EnvDefaultFunc("INSTANT_PROXY_PASSWORD", nil),
+		},
+	}
+}
+
+func configureContext(_ context.Context, data *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	client := ipac.NewClient(
+		data.Get("username").(string),
+		data.Get("password").(string),
+	)
+	err := client.Authenticate()
+	if err != nil {
+		return nil, diag.FromErr(err)
+	}
+	return client, nil
 }
